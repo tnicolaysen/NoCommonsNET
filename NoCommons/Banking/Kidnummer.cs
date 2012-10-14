655272e4875efd5ed8ffaf2fa4361e4f744f2764
Torstein Nicolaysen <tnicolaysen@gmail.com>
2012-10-14 16:14:46 +0200

    Cleaned up the design and API for KidnummerValidaotr and Kidnummer
    Added more XMLdoc
    Made the constructor for Kidnummer protected internal as to avoid
    confusion.
    The only way to create an instance of Kidnummer is to use the
    GetKidnummer()method in KidnummerValidator. This ensures that the user
    gets a validated instance back.
    This might be improved as it conflicts with SRP (both validator and
    factory)

--- NoCommons/Banking/Kidnummer.cs
+++ NoCommons/Banking/Kidnummer.cs
@@ -9,7 +9,19 @@ namespace NoCommons.Banking
     /// </summary>
     public class Kidnummer : StringNumber
     {
-        public Kidnummer(string kontonummer) : base(kontonummer)
+        /// <summary>
+        /// <p>
+        /// This constructor is meant to be used by <see cref="KidnummerValidator"/>.
+        /// The reason for this is that a user of this lib should not be allowed
+        /// to create an instance of this class that is invalid.
+        /// </p>
+        /// 
+        /// <p>
+        /// To create a new instance of this class, use <see cref="KidnummerValidator.GetKidnummer" />
+        /// </p>
+        /// </summary>
+        /// <param name="kontonummer">A valid KID-nummer</param>
+        protected internal Kidnummer(string kontonummer) : base(kontonummer)
         {
         }
     }
--- NoCommons/Banking/KidnummerValidator.cs
+++ NoCommons/Banking/KidnummerValidator.cs
@@ -4,60 +4,61 @@ using NoCommons.Common;
 namespace NoCommons.Banking
 {
     /// <summary>
-    /// Validates <see cref="Kidnummer"/>.
+    /// Validates a KID-nummer string
+    /// and a factory method to obtain a validated instance of a <see cref="Kidnummer"/>
     /// </summary>
-    public class KidnummerValidator : StringNumberValidator {
-
-        public const string LenghtErrorMessage = "A valid kidnummer is between 2 and 25 digits";
+    public class KidnummerValidator : StringNumberValidator
+    {
+        internal const string LenghtErrorMessage = "A valid kidnummer is between 2 and 25 digits";
 
         /// <summary>
         /// Return true if the provided String is a valid KID-nummmer.
         /// </summary>
         /// <param name="kidnummer">String representing a Kidnummer</param>
         /// <returns>True if valid</returns>
-        public static bool IsValid(String kidnummer) {
-            try {
+        public static bool IsValid(String kidnummer)
+        {
+            try
+            {
                 GetKidnummer(kidnummer);
                 return true;
-            } catch (ArgumentException) {
+            }
+            catch (ArgumentException)
+            {
                 return false;
             }
         }
 
-        /**
-	     * Returns an object that represents a Kidnummer.
-	     * 
-	     * @param kidnummer
-	     *            A String containing a Kidnummer
-	     * @return A Kidnummer instance
-	     * @throws IllegalArgumentException
-	     *             thrown if String contains an invalid Kidnummer
-	     */
-        public static Kidnummer GetKidnummer(string kidnummer) {
+        /// <param name="kidnummer">A string containing a KID number</param>
+        /// <exception cref="ArgumentException">If string is an invalid KID number</exception>
+        /// <returns>Returns an object that represents a Kidnummer. </returns>
+        public static Kidnummer GetKidnummer(string kidnummer)
+        {
             ValidateSyntax(kidnummer);
             ValidateChecksum(kidnummer);
             return new Kidnummer(kidnummer);
         }
 
-        internal static void ValidateSyntax(string kidnummer) {
+        internal static void ValidateSyntax(string kidnummer)
+        {
             ValidateAllDigits(kidnummer);
             ValidateLengthInRange(kidnummer, 2, 25);
         }
 
-        private static void ValidateLengthInRange(string kidnummer, int i, int j) {
-            if (kidnummer == null || kidnummer.Length < i || kidnummer.Length > j) {
+        private static void ValidateLengthInRange(string kidnummer, int i, int j)
+        {
+            if (kidnummer == null || kidnummer.Length < i || kidnummer.Length > j)
                 throw new ArgumentException(LenghtErrorMessage);
-            }
         }
 
-        internal static void ValidateChecksum(String kidnummer) {
+        private static void ValidateChecksum(String kidnummer)
+        {
             StringNumber k = new Kidnummer(kidnummer);
             int kMod10 = CalculateMod10CheckSum(GetMod10Weights(k), k);
             int kMod11 = CalculateMod11CheckSum(GetMod11Weights(k), k);
-            if (kMod10 != k.GetChecksumDigit() && kMod11 != k.GetChecksumDigit()) {
-                throw new ArgumentException(InvalidChecksumErrorMessage + kidnummer);
-            }
-        }
 
+            if (kMod10 != k.GetChecksumDigit() && kMod11 != k.GetChecksumDigit())
+                throw new ArgumentException(InvalidChecksumErrorMessage + kidnummer);
+        }
     }
 }
