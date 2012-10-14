ef0c9b7a671170387763c2042908a89160d9c4a6
Torstein Nicolaysen <tnicolaysen@gmail.com>
2012-10-14 16:32:49 +0200

    Worked on readability and formatting on StringNumberValidator

--- NoCommons/Common/StringNumberValidator.cs
+++ NoCommons/Common/StringNumberValidator.cs
@@ -5,74 +5,99 @@ namespace NoCommons.Common
 {
     public abstract class StringNumberValidator
     {
-        public const string InvalidChecksumErrorMessage = "Invalid checksum : ";
-        public const string SyntaxErrorMessage = "Only digits are allowed : ";
+        internal const string InvalidChecksumErrorMessage = "Invalid checksum : ";
+        internal const string SyntaxErrorMessage = "Only digits are allowed : ";
 
         private static readonly int[] BaseMod11Weights = new[] {2, 3, 4, 5, 6, 7};
-        
-       protected static int CalculateMod11CheckSum(int[] weights, StringNumber number) {
-          int c = CalculateChecksum(weights, number, false) % 11;
-          if (c == 1) {
-             throw new ArgumentException(InvalidChecksumErrorMessage + number);
-          }
-          return c == 0 ? 0 : 11 - c;
-       }
-    
-       protected static int CalculateMod10CheckSum(int[] weights, StringNumber number) {
-          int c = CalculateChecksum(weights, number, true) % 10;
-          return c == 0 ? 0 : 10 - c;
-       }
 
-       private static int CalculateChecksum(int[] weights, StringNumber number, bool tverrsum) {
-          int checkSum = 0;
-          for (int i = 0; i < weights.Length; i++) {
-             int product = weights[i] * number.GetAt(weights.Length - 1 - i);
-             if (tverrsum) {
-                checkSum += (product > 9 ? product - 9 : product);
-             } else {
-                checkSum += product;
-             }
-          }
-          return checkSum;
-       }
+        protected static int CalculateMod11CheckSum(int[] weights, StringNumber number)
+        {
+            int checksum = CalculateChecksum(weights, number, false)%11;
+            if (checksum == 1) 
+                throw new ArgumentException(InvalidChecksumErrorMessage + number);
 
-       protected static void ValidateLengthAndAllDigits(string numberString,
-                                                        int length) {
-          if (numberString == null || numberString.Length != length) {
-             throw new ArgumentException(SyntaxErrorMessage + numberString);
-          }
-          ValidateAllDigits(numberString);
-       }
+            if (checksum == 0) 
+                return 0;
+            else 
+                return 11 - checksum;
+        }
 
-       protected static void ValidateAllDigits(string numberString) {
-           if (numberString == null || numberString.Length <= 0) {
-             throw new ArgumentException(SyntaxErrorMessage + numberString);
-          }
-           if (numberString.Any(t => !char.IsDigit((t)))) {
-               throw new ArgumentException(SyntaxErrorMessage + numberString);
-           }
-       }
+        protected static int CalculateMod10CheckSum(int[] weights, StringNumber number)
+        {
+            int checksum = CalculateChecksum(weights, number, true)%10;
+            
+            if (checksum == 0) 
+                return 0;
+            else 
+                return 10 - checksum;
+        }
 
-        protected static int[] GetMod10Weights(StringNumber k) {
-          var weights = new int[k.GetLength() - 1];
-          for (int i = 0; i < weights.Length; i++) {
-             if ((i % 2) == 0) {
-                weights[i] = 2;
-             } else {
-                weights[i] = 1;
-             }
-          }
-          return weights;
-       }
+        private static int CalculateChecksum(int[] weights, StringNumber number, bool tverrsum)
+        {
+            int checksum = 0;
+            for (int i = 0; i < weights.Length; i++)
+            {
+                int weight = weights[i];
+                int currentNumber = number.GetAt(weights.Length - 1 - i);
+                int product = weight*currentNumber;
 
-       protected static int[] GetMod11Weights(StringNumber k) {
-          var weights = new int[k.GetLength() - 1];
-          for (int i = 0; i < weights.Length; i++) {
-             int j = i % BaseMod11Weights.Length;
-             weights[i] = BaseMod11Weights[j];
-          }
-          return weights;
-       }
+                if (tverrsum)
+                {
+                    if (product > 9) checksum += product - 9;
+                    else checksum += product;
+                }
+                else 
+                {
+                    checksum += product;
+                }
+            }
+            return checksum;
+        }
 
+        protected static void ValidateLengthAndAllDigits(string numberString, int length)
+        {
+            if (numberString == null || numberString.Length != length)
+                throw new ArgumentException(SyntaxErrorMessage + numberString);
+
+            ValidateAllDigits(numberString);
+        }
+
+        protected static void ValidateAllDigits(string numberString)
+        {
+            if (string.IsNullOrEmpty(numberString))
+                throw new ArgumentException(SyntaxErrorMessage + numberString);
+
+            if (numberString.Any(NotADigit))
+                throw new ArgumentException(SyntaxErrorMessage + numberString);
+        }
+
+        private static bool NotADigit(char c)
+        {
+            return !char.IsDigit((c));
+        }
+
+        protected static int[] GetMod10Weights(StringNumber k)
+        {
+            var weights = new int[k.GetLength() - 1];
+            for (int i = 0; i < weights.Length; i++)
+            {
+                if ((i%2) == 0) 
+                    weights[i] = 2;
+                else 
+                    weights[i] = 1;
+            }
+            return weights;
+        }
+
+        protected static int[] GetMod11Weights(StringNumber k)
+        {
+            var weights = new int[k.GetLength() - 1];
+            for (int i = 0; i < weights.Length; i++)
+            {
+                int j = i%BaseMod11Weights.Length;
+                weights[i] = BaseMod11Weights[j];
+            }
+            return weights;
+        }
     }
 }
