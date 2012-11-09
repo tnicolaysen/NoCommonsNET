907ad31cbeb47096d16bcd08fbd88d0ecb6bd756
John Korsnes <johnkors@gmail.com>
2012-11-09 17:24:06 +0100

    MailDataLoader init. Failing unit tests

---
+++ NoCommons.Tests/Mail/MailDataLoaderTests.cs
@@ -0,0 +1,64 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Threading;
+using NUnit.Framework;
+using NoCommons.Mail;
+
+namespace NoCommons.Tests.Mail
+{
+public class MailDataLoaderTest {
+
+	public void setUpLocaleAndLoadData() 
+    {
+		//Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("nb-NO");
+        var f = Resources.Resources.tilbud5;
+        using(var s = GenerateStreamFromString(f))
+        {
+            MailDataLoader.loadFromInputStream(s); 
+        }
+		
+	}
+
+    public Stream GenerateStreamFromString(string s)
+    {
+        var stream = new MemoryStream();
+        var writer = new StreamWriter(stream);
+        writer.Write(s);
+        writer.Flush();
+        stream.Position = 0;
+        return stream;
+    }
+
+	[Test]
+	public void testAntallPoststed()
+	{
+	    setUpLocaleAndLoadData();
+		Assert.AreEqual(1852, MailValidator.getAntallPoststed());
+	}
+
+	[Test]
+	public void testAntallPostnummer() {
+        setUpLocaleAndLoadData();
+		Assert.AreEqual(4586, MailValidator.getAntallPostnummer());
+	}
+
+	[Test]
+	public void testAntallPostnummerForHamar() {
+        setUpLocaleAndLoadData();
+		var options = MailValidator.getPostnummerForPoststed("HAMAR");
+		Assert.AreEqual(17, options.Count);
+	}
+
+	[Test]
+	public void testPoststedForPostnummer2315() {
+        setUpLocaleAndLoadData();
+		Assert.AreEqual("HAMAR", MailValidator.getPoststedForPostnummer("2315").ToString());
+	}
+
+	[Test]
+	public void testLoadDataFromResource() {
+		bool success = MailDataLoader.loadFromResource();
+		Assert.IsTrue(success);
+	}
+}
+}
---
+++ NoCommons/Mail/MailDataLoader.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace NoCommons.Mail
+{
+    public class MailDataLoader
+    {
+        public static void loadFromInputStream(Stream ist)
+        {
+            if (ist == null)
+            {
+                throw new ArgumentException();
+            }
+            var poststedMap = new Dictionary<Poststed, List<Postnummer>>();
+            var postnummerMap = new Dictionary<Postnummer, Poststed>();
+            using (var isr = new StreamReader(ist, Encoding.UTF8))
+            {
+                string line;
+                while ((line = isr.ReadLine()) != null)
+                {
+                    var st = line.Split('\t');
+                    var pn = MailValidator.getPostnummer(st[0]);
+                    var ps = new Poststed(st[1]);
+
+                    // add to poststedMap
+                    var postnummerList = new List<Postnummer>();
+                    if (poststedMap.ContainsKey(ps))
+                    {
+                        postnummerList = poststedMap[ps];
+                    }
+                    if (!postnummerList.Contains(pn))
+                    {
+                        postnummerList.Add(pn);
+                    }
+
+
+                    poststedMap.Add(ps, postnummerList);
+
+                    // add to postnummerMap
+                    var alreadyExists = postnummerMap.ContainsKey(pn);
+                    
+                    if (alreadyExists)
+                    {
+
+                    }
+                    else
+                    {
+                        postnummerMap.Add(pn, ps);
+                    }
+
+                }
+            }
+            MailValidator.setPoststedMap(poststedMap);
+            MailValidator.setPostnummerMap(postnummerMap);
+        }
+
+        public static bool loadFromResource()
+        {
+            bool success = false;
+            Stream ist = null;
+            try
+            {
+                ist = GenerateStreamFromString(Resources.Resources.tilbud5);
+
+                loadFromInputStream(ist);
+                success = true;
+            }
+            catch (IOException e)
+            {
+                // ignore
+            }
+            finally
+            {
+                try
+                {
+                    if (ist != null)
+                    {
+                        ist.Close();
+                    }
+                }
+                catch (IOException e)
+                {
+                    // ignore
+                }
+            }
+            return success;
+        }
+
+        public static Stream GenerateStreamFromString(string s)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+
+    }
+}
