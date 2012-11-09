e82272719fe214b7e7c900e83a3655f7b1c812d1
John Korsnes <johnkors@gmail.com>
2012-11-09 15:49:51 +0100

    cleanup, c#

--- NoCommons/Mail/MailValidator.cs
+++ NoCommons/Mail/MailValidator.cs
@@ -4,62 +4,73 @@ using NoCommons.Common;
 
 namespace NoCommons.Mail
 {
-public class MailValidator : StringNumberValidator {
+    public class MailValidator : StringNumberValidator
+    {
+        private const int POSTNUMMER_LENGTH = 4;
+        private static Dictionary<Poststed, List<Postnummer>> poststedMap = new Dictionary<Poststed, List<Postnummer>>();
+        private static Dictionary<Postnummer, Poststed> postnummerMap = new Dictionary<Postnummer, Poststed>();
 
-	private const int POSTNUMMER_LENGTH = 4;
-    private static Dictionary<Poststed, List<Postnummer>> poststedMap = new Dictionary<Poststed, List<Postnummer>>();
-    private static Dictionary<Postnummer, Poststed> postnummerMap = new Dictionary<Postnummer, Poststed>();
+        public static void setPostnummerMap(Dictionary<Postnummer, Poststed> aPostnummerMap)
+        {
+            postnummerMap = aPostnummerMap;
+        }
 
-	public static void setPostnummerMap(Dictionary<Postnummer, Poststed> aPostnummerMap) {
-		postnummerMap = aPostnummerMap;
-	}
+        public static void setPoststedMap(Dictionary<Poststed, List<Postnummer>> aPoststedMap)
+        {
+            poststedMap = new Dictionary<Poststed, List<Postnummer>>(aPoststedMap);
+        }
 
-	public static void setPoststedMap(Dictionary<Poststed, List<Postnummer>> aPoststedMap) {
-		poststedMap = new Dictionary<Poststed, List<Postnummer>>(aPoststedMap);
-	}
+        public static int getAntallPoststed()
+        {
+            return poststedMap.Count;
+        }
 
-	public static int getAntallPoststed() {
-      return poststedMap.Count;
-	}
+        public static int getAntallPostnummer()
+        {
+            return postnummerMap.Count;
+        }
 
-	public static int getAntallPostnummer() {
-      return postnummerMap.Count;
-	}
+        public static bool isValidPostnummer(string postnummer)
+        {
+            try
+            {
+                getPostnummer(postnummer);
+                return true;
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+        }
 
-	public static bool isValidPostnummer(string postnummer) {
-		try {
-			getPostnummer(postnummer);
-			return true;
-		} catch (ArgumentException e) {
-			return false;
-		}
-	}
+        internal static Postnummer getPostnummer(string postnummer)
+        {
+            validateSyntax(postnummer);
+            return new Postnummer(postnummer);
+        }
 
-	public static Postnummer getPostnummer(string postnummer) {
-		validateSyntax(postnummer);
-		return new Postnummer(postnummer);
-	}
+        public static List<Postnummer> getPostnummerForPoststed(string poststed)
+        {
+            var p = new Poststed(poststed);
+            List<Postnummer> postnummerList;
+            var found = poststedMap.TryGetValue(p, out postnummerList);
+            return (found ? postnummerList : new List<Postnummer>());
+        }
 
-	public static List<Postnummer> getPostnummerForPoststed(string poststed) {
-		var p = new Poststed(poststed);
-		List<Postnummer> postnummerList;
-        var found = poststedMap.TryGetValue(p, out postnummerList);
-        return (found ? postnummerList :  new List<Postnummer>());
-	}
+        private static void validateSyntax(string postnummer)
+        {
+            ValidateLengthAndAllDigits(postnummer, POSTNUMMER_LENGTH);
+        }
 
-	private static void validateSyntax(string postnummer) {
-		ValidateLengthAndAllDigits(postnummer, POSTNUMMER_LENGTH);
-	}
-
-	public static Poststed getPoststedForPostnummer(string postnummer) {
-		Poststed result = null;
-		var pn = getPostnummer(postnummer);
-		if (postnummerMap.ContainsKey(pn)) {
-			result = postnummerMap[pn];
-		}
-		return result;
-	}
-
-}
-
+        public static Poststed getPoststedForPostnummer(string postnummer)
+        {
+            Poststed result = null;
+            var pn = getPostnummer(postnummer);
+            if (postnummerMap.ContainsKey(pn))
+            {
+                result = postnummerMap[pn];
+            }
+            return result;
+        }
+    }
 }
